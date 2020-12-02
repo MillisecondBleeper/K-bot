00ca9f95782e958fcd44b7e227becb046003d533
Darkuss420 <64544951+Darkuss420@users.noreply.github.com>
2020-12-02 18:37:06 +0300

    Update main-code.js

--- main-code.js
+++ main-code.js
@@ -50,10 +50,8 @@ client.on('message', async(message) => {
         if (command === "ban") {
             const banned_user = message.mentions.members.first();
             if (canBan(message.member) && banned_user !== undefined) {
-                function e(){ 
-                    message.split(' ', 3);
-                }
-                const reason = (e().size === 3)?e()[2]:'';
+                var e = message.content.split(' ', 3);
+                const reason = (e.size === 3)?e[2]:'';
                 const confirm_embed = new Discord.MessageEmbed()
                     .setColor('#1F8B4C')
                     .setTitle(`Are you sure you want to ban this user?`)
